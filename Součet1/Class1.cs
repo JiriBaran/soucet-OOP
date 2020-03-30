7b755f418b2a70d448a41b7447a7d501e0fe30e9
Jiří Baran <jurka.baran@seznam.cz>
2020-03-30 17:54:04 +0200

    Přidejte soubory projektu.

---
+++ Součet1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Součet1
+{
+    class Numbers
+    {
+        public double Soucet(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Součet1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Součet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine(numbers.Soucet(6, 8));
+            Console.ReadKey();
+
+        }
+    }
+}
